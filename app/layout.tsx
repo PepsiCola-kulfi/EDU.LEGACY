f0618570a70005ac13e6f9e84148366f4d3a6cf1
Debanjan Mondal <debanjanmondalk2005@gmail.com>
2025-02-09 16:45:15 +0530

    FE

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,43 @@
+// import localFont from "next/font/local";
+// import { ThemeProvider } from 'next-themes'
+// import "./globals.css";
+// import { SmartWillProvider } from "@/context/SmartWillContext";
+
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <SmartWillProvider>
+//       <html lang="en" >
+//         <body className={`${geistSans.variable} ${geistMono.variable}`}>
+         
+//           <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+//             {children}
+//           </ThemeProvider>
+//         </body>
+//       </html>
+//     </SmartWillProvider>
+//   );
+// }
 import localFont from "next/font/local";
 import { ThemeProvider } from 'next-themes'
 import "./globals.css";
 import { SmartWillProvider } from "@/context/SmartWillContext";
-
+import { Poppins } from 'next/font/google'
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -14,7 +49,7 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
-
+const poppins = Poppins({ weight: ["400"], subsets: ['latin'] , variable: '--font-poppins'})
 export default function RootLayout({
   children,
 }: {
@@ -23,7 +58,7 @@ export default function RootLayout({
   return (
     <SmartWillProvider>
       <html lang="en" >
-        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <body className={` ${poppins.className}`}>
          
           <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
             {children}
